ea0625ce3311320fde4e56b358087785c6e03adc
SahilLodhe <sahillodhe17@gmail.com>
2023-04-05 23:02:30 +0530

    s

--- reactdom/src/App.js
+++ reactdom/src/App.js
@@ -1,21 +1,18 @@
+import { useState } from 'react';
 import './App.css'
-import { Home } from "./pages/Home"
-import { Contact } from "./pages/Contact"
-import { Menu } from "./pages/Menu"
-import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom" // router determines which specific parts of website you want to change
-import { Navbar } from './pages/Navbar'
+
 function App() {
+  const topComponent = () => {
+    const [state, setState] = useState()
+    return (
+      <div>
+        MiddleC
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/menu" element={<Menu />} />
-           <Route path="/contact" element={<Contact />} /> 
-           <Route path="*" element={<h1>PAGE NOT FOUND</h1>} /> {/* this the disaply content for the rest of the pages */}
-        </Routes>
-      </Router>
+      
     </div>
   );
 }
